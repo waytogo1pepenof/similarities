aa10951324023e36e9b60f7718a9bceeddf1af9a
pepenof <pepenof2jinjib@gmail.com>
2026-07-17 23:26:01 +0900

    (Q5)コッホ曲線の座標を基調とした作成

--- prog_c/math_to_koch/q5.c
+++ prog_c/math_to_koch/q5.c
@@ -1,28 +1,30 @@
 #include <stdio.h>
 #include <stdlib.h>
+#define _USE_MATH_DEFINES
 #include <math.h>
 
 int koch (double x0, double y0, double x1, double y1, int n, FILE *fp) {
     double fin_x[5], fin_y[5];
     double whole_len_x, whole_len_y;
-    // 変数については別記画像ファイルを参照（list.png）
+
+    double cos_sixty = 1.0 / 2.0;
+    double sin_sixty = sqrt(3.0) / 2.0;
+
     whole_len_x = x1 - x0;
     whole_len_y = y1 - y0;
 
     fin_x[0] = x0;
-    fin_x[1] = whole_len_x / 3;
-    fin_x[3] = whole_len_x * (2/3);
-    
-    fin_y[0] = y0;
-    fin_y[1] = whole_len_y / 3;
-    fin_y[3] = whole_len_y * (2/3);
-
-    double sixty_deg = M_PI / 3.0;
-    fin_x[2] = fin_x[3] * cos(sixty_deg) - fin_y[3] * sin(sixty_deg);
-    fin_y[2] = fin_x[3] * sin(sixty_deg) + fin_y[3] * cos(sixty_deg);
-
+    fin_x[1] = x0 + whole_len_x / 3.0;
+    fin_x[3] = x0 + whole_len_x * 2.0/3.0;
     fin_x[4] = x1;
+
+    fin_y[0] = y0;
+    fin_y[1] = y0 + whole_len_y / 3.0;
+    fin_y[3] = y0 + whole_len_y * 2.0/3.0;
     fin_y[4] = y1;
+
+    fin_x[2] = fin_x[1] + ((fin_x[3] - fin_x[1]) * cos_sixty - (fin_y[3] - fin_y[1]) * sin_sixty);
+    fin_y[2] = fin_y[1] + ((fin_y[3] - fin_y[1]) * cos_sixty + (fin_x[3] - fin_x[1]) * sin_sixty);
 
     if (n < 0) {
         printf("[invalid: please input n > 0]");
@@ -30,15 +32,15 @@ int koch (double x0, double y0, double x1, double y1, int n, FILE *fp) {
     }
 
     if (n == 0) {
-        fprintf(fp, "%lf\t%lf\n", fin_x[0], fin_y[0]);
-        fprintf(fp, "%lf\t%lf\n", fin_x[4], fin_y[4]);
+        fprintf(fp, "%lf\t%lf\n", x0, y0);
+        return 0;
     } else {
-        for (int i = 0; i < 5; i++) {
+        for (int i = 0; i < 4; i++) {
             koch(fin_x[i], fin_y[i], fin_x[i+1], fin_y[i+1], n - 1, fp);
         }
-        fprintf(fp, "\n");
     }
 
+    return 0;
 }
 
 int main() {
@@ -50,7 +52,10 @@ int main() {
         exit(1);
     }
 
-    koch(0, 0, 12, 0, 3, fp);
+    int depth = 5;
+
+    koch(0.0, 0.0, 300.0, 0.0, depth, fp);
+    fprintf(fp, "%lf\t%lf\n", 300.0, 0.0);
     
     fclose(fp);
     return 0;
